f4ec0b67ac4d2bec17c7095328bbeca457674ba4
CraciDenisStefan <crdenis13@gmail.com>
2023-08-31 17:07:34 +0200

    componenti about us e askus form

---
+++ src/components/AskUs.vue
@@ -0,0 +1,31 @@
+<script>
+import axios from 'axios';
+import { store } from '../store.js'
+import AppLoader from '../components/AppLoader.vue';
+
+export default {
+    name:'AskUs',
+    components:{
+        AppLoader,
+    },
+    data(){
+        return{
+            store,
+        }
+    },
+    created(){
+       
+    },
+    methods:{
+
+    }
+}
+</script>
+<template >
+    <div>
+        
+    </div>
+</template>
+<style lang="scss">
+    
+</style>
