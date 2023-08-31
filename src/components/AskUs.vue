eb7941e1d1b1f4c758a876606bda3200b8c08853
CraciDenisStefan <crdenis13@gmail.com>
2023-08-31 17:18:58 +0200

    Form creata ( solo form )

--- src/components/AskUs.vue
+++ src/components/AskUs.vue
@@ -22,8 +22,31 @@ export default {
 }
 </script>
 <template >
-    <div>
-        
+    <div class="container">
+        <div class="row">
+
+            <form action="" method="POST">
+                        <div class="class-group">
+                            <label class="control-label">Nome</label>
+                            <input type="text" id="name" name="name"  placeholder="Name" >
+                        </div>
+                        <div class="class-group">
+                            <label class="control-label">Cognome</label>
+                            <input type="text" id="surname" name="surname"  placeholder="Surname" >
+                        </div>
+                        <div class="class-group">
+                            <label class="control-label">Email</label>
+                            <input type="email" class="form-control"  placeholder="name@example.com">
+                        </div>
+                        <div class="class-group">
+                            <label class="control-label">Domanda</label>
+                            <textarea class="form-control" rows="3"></textarea>
+                        </div>      
+                        <div class="class-group my-3">
+                            <button type="submit" class="btn btn-primary btn-success">Invia</button>
+                        </div>
+                    </form>
+        </div>     
     </div>
 </template>
 <style lang="scss">
