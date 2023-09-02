e10863bd1151d33920449cee59429cea8715a9f5
CraciDenisStefan <crdenis13@gmail.com>
2023-09-02 12:11:25 +0200

    Hamburger menu ok

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import "bootstrap/dist/js/bootstrap.js";
 import lottie from 'lottie-web';
 import { defineElement } from 'lord-icon-element';
 defineElement(lottie.loadAnimation);
